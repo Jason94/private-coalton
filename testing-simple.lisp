3492a81939790a7cafb17b7e5c83c51d414edbf7
Jason Walker <jason0@pm.me>
2024-12-31 18:01:54 -0500

    Move out Environment and finish rough Bank ex

---
+++ testing-simple.lisp
@@ -0,0 +1,72 @@
+(in-package :cl-user)
+(defpackage :private-coalton.testing-simple
+  (:use
+   #:coalton
+   #:coalton-prelude
+   #:coalton-library/result
+   #:coalton-library/monad/state
+   #:coalton-library/monad/environment
+   #:coalton-library/monad/resultt)
+  (:local-nicknames
+   (#:s #:coalton-library/string)
+   (#:m #:coalton-library/ord-map)))
+(in-package :private-coalton.testing-simple)
+
+(coalton-toplevel
+  (define (trace-tuple label-a label-b tup)
+    (match tup
+      ((Tuple a b)
+       (progn
+        (traceobject label-a a)
+        (traceobject label-b b))))))
+
+(coalton-toplevel
+  (repr :enum)
+  (define-type Color Red Blue Yellow)
+
+  (declare rotate (Color -> Color))
+  (define (rotate c)
+    (match c
+      ((Red) Blue)
+      ((Blue) Yellow)
+      ((Yellow) Red))))
+
+(coalton-toplevel
+  (declare RGB (Env Color (List Integer)))
+  (define RGB
+    (do
+      (color <- ask)
+      (pure
+        (match color
+          ((Red) (make-list 255 0 0))
+          ((Blue) (make-list 0 255 0))
+          ((Yellow) (make-list 0 0 255)))))))
+
+(coalton-toplevel
+  (define (ex-colors)
+   (run-env
+    (do
+     (ints-rep <- RGB)
+     (let _ = (traceobject "Ints rep" ints-rep))
+     (next-color <- (asks-envT rotate))
+     (let _ = (traceobject "Next color" next-color))
+     (_ints-rep2 <- (local-envT (const next-color) RGB))
+      ;; (let _ = (traceobject "Ints rep 2" ints-rep2))
+      (pure Unit))
+    Red)))
+
+(coalton-toplevel
+  (declare print-plus10 (Env Integer Unit))
+  (define print-plus10
+    (do
+     (x <- ask)
+     (let _ = (traceobject "X" x))
+     (pure Unit))))
+
+(coalton
+ (run-env
+  (do
+   (_ <- print-plus10)
+   (_ <- (local (+ 5) print-plus10))
+   (pure Unit))
+  0))
--- testing.lisp
+++ testing.lisp
@@ -3,61 +3,29 @@
   (:use
    #:coalton
    #:coalton-prelude
+   #:coalton-library/result
    #:coalton-library/monad/state
-   #:private-coalton.identity
-   #:private-coalton.monad-transformer
-   #:private-coalton.environment)
+   #:coalton-library/monad/environment
+   #:coalton-library/monad/resultt)
   (:local-nicknames
+   (#:s #:coalton-library/string)
    (#:m #:coalton-library/ord-map)))
 (in-package :private-coalton.testing)
 
 (coalton-toplevel
-  (repr :enum)
-  (define-type Color Red Blue Yellow)
-
-  (declare rotate (Color -> Color))
-  (define (rotate c)
-    (match c
-      ((Red) Blue)
-      ((Blue) Yellow)
-      ((Yellow) Red))))
-
-(coalton-toplevel
-  (declare RGB (Env Color (List Integer)))
-  (define RGB
-    (do
-      (color <- ask)
-      (pure
-        (match color
-          ((Red) (make-list 255 0 0))
-          ((Blue) (make-list 0 255 0))
-          ((Yellow) (make-list 0 0 255)))))))
-
-(cl:defun ex-colors ()
-  (coalton
-   (run-env
-    (do
-     (ints-rep <- RGB)
-     (let _ = (traceobject "Ints rep" ints-rep))
-      (next-color <- (asks rotate))
-      (ints-rep2 <- (local (const next-color) RGB))
-      (let _ = (traceobject "Ints rep 2" ints-rep2))
-      (pure Unit))
-    Red)))
+  (define (trace-tuple label-a label-b tup)
+    (match tup
+      ((Tuple a b)
+       (progn
+        (traceobject label-a a)
+        (traceobject label-b b))))))
 
 ;;;
 ;;; Bank Example
 ;;;
 
-(coalton-toplevel
-  (declare opt->result (:err -> Optional :a -> Result :err :a))
-  (define (opt->result failure opt)
-    (match opt
-      ((None) (Err failure))
-      ((Some a) (Ok a)))))
-
 (coalton-toplevel (define-type-alias AccountName String)
-  (define-type-alias Balance     Integer)
+  (define-type-alias Balance Integer)
 
   (define-struct Configuration
     (minimum-balance      "Minimum balance that must be floated by an account."                          Balance)
@@ -74,7 +42,7 @@
 
   (define-type-alias BankM
     (EnvT Configuration BankStateM))
-  
+
   (declare run-bankM (BankM :val -> Configuration -> BankState -> Tuple BankState :val))
   (define (run-bankM bankm conf initial-balance)
     (run (run-envT bankm conf) initial-balance)))
@@ -82,37 +50,81 @@
 (coalton-toplevel
   (declare get-account (AccountName -> BankState -> Result String Account))
   (define (get-account account-name accounts)
-    (opt->result "Could not find account" (m:lookup accounts account-name))))
+    (opt->result "Could not find account" (m:lookup accounts account-name)))
+
+  (declare account-valid? (Account -> BankM (Result String Account)))
+  (define (account-valid? account)
+    (do
+     (minimum-balance <- (asks .minimum-balance))
+     (if (>= (.balance account) minimum-balance)
+         (pure (Ok account))
+         (pure (Err "Account balance is too low!")))))
+
+  (declare set-account (Account -> BankM (Result String Account)))
+  (define (set-account acc)
+    "Update/insert an account and return it for convenience."
+    (do
+     (modify (fn (mp)
+               (m:insert-or-replace mp (.name acc) acc)))
+     (pure (Ok acc)))))
 
 (coalton-toplevel
   (declare create-account (AccountName -> Balance -> BankM (Result String Account)))
   (define (create-account name initial-balance)
-    (do
-     (accounts <- get)
-     ()
+    (run-resultT
+     (do
+      (accounts <- get)
+      (ResultT
+       (match (get-account name accounts)
+         ((Err _) (pure (Ok Unit)))
+         ((Ok _) (pure (Err "Account already exists")))))
+      (let unvalidated-account = (Account name initial-balance))
+      (account <- (ResultT (account-valid? unvalidated-account)))
+      (ResultT (set-account account)))))
 
   (declare deposit (AccountName -> Integer -> BankM (Result String Account)))
   (define (deposit account-name amount)
-    (do
-      (accounts <- get)
-      (let account? = (get-account account-name accounts))
-      (sequence (map
-                 (fn ((Account name balance))
-                   (pure (Account name (+ balance amount))))
-                 account?))))
+    "Deposit AMOUNT into account with ACCOUNT-NAME and return the Account for convenience."
+    (run-resultT
+     (do
+      (err-ifT (< amount 0) "Cannot deposit negative amount")
+      ((Account _ balance) <- (ResultT (map (get-account account-name) get)))
+      (let new-account = (Account account-name (+ balance amount)))
+      (ResultT (set-account new-account))))))
 
+(coalton-toplevel
   (declare withdraw (AccountName -> Integer -> BankM (Result String Account)))
-  (define (withdraw account-name _amount)
-    (do
-      (accounts <- get)
-      (let _account? = (get-account account-name accounts))
-      (_protection? <- (asks .overdraft-protection))
-      (_minimum <- (asks .minimum-balance))
-      (pure (Err "")))))
+  (define (withdraw account-name amount)
+    "Withdraw AMOUNT from account with ACCOUNT-NAME, returning the Account for convenience."
+    (run-resultT
+     (do
+      (err-ifT (< amount 0) "Cannot withdraw negative amount")
+      (acc <- (ResultT (map (get-account account-name) get)))
+      (map-errT (fn (msg)
+                  (s:concat "Cannot withdraw from an invalid account ["
+                            (s:concat msg "]")))
+                (ResultT (account-valid? acc)))
+      (let new-account = (Account account-name (- (.balance acc) amount)))
+      (protection? <- (lift (asks .overdraft-protection)))
+      (minimum <- (lift (asks .minimum-balance)))
+      (if (and protection?
+               (< (.balance new-account) minimum))
+          (ResultT (pure (Err "Cannot withdraw below minimum balance with overdraft protection.")))
+          (pure Unit))
+      (ResultT (set-account new-account))))))
 
 (coalton
+ (trace-tuple
+  "Accounts" "Result"
   (run-bankM
-    (do
-      (deposit "Checking" 10))
-    (Configuration 10 True)
-    m:empty))
+   (run-resultT
+   (do
+    (ResultT (create-account "Checking" 100))
+    (ResultT (deposit "Checking" 10))
+    (ResultT (withdraw "Checking" 20))
+    (ResultT (withdraw "Checking" 85))
+    (ResultT (deposit "Checking" 2))
+    (ResultT (withdraw "Checking" 5))
+     ))
+   (Configuration 10 False)
+   m:empty)))
